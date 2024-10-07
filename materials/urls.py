12ad6dff2b60cb3caaf88e3b20f0d451b6d90a30
dimapb <1st.trillionaire.dm@gmail.com>
2024-10-07 20:01:53 +0300

    ViewSet для модели курса

--- materials/urls.py
+++ materials/urls.py
@@ -1,7 +1,13 @@
 from materials.apps import MaterialsConfig
+from rest_framework.routers import DefaultRouter
+
+from materials.views import CourseViewSet
 
 app_name = MaterialsConfig.name
 
+router = DefaultRouter()
+router.register('courses', CourseViewSet, basename='courses')
+
 urlpatterns = [
 
-]
+] + router.urls
