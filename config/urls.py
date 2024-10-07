a06101262b62937e44094e4bb760e6646fb54855
dimapb <1st.trillionaire.dm@gmail.com>
2024-10-07 19:08:01 +0300

    Добавление моделей курса и урока

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls', namespace='users')),
+    path('', include('materials.urls', namespace='materials')),
 ]
 
 if settings.DEBUG:
