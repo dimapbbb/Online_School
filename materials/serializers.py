1a8badeabea0dacc077c148bc8252ce73cdfaf43
dimapb <1st.trillionaire.dm@gmail.com>
2024-10-07 20:32:42 +0300

    Дженерики для модели урока

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from materials.models import Course
+from materials.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -8,3 +8,10 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ('id', 'title', 'description')
+
+
+class LessonSerializer(serializers.ModelSerializer):
+
+     class Meta:
+         model = Lesson
+         fields = ('id', 'title', 'description', 'link_to_video', 'course')
--- materials/urls.py
+++ materials/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path
+
 from materials.apps import MaterialsConfig
 from rest_framework.routers import DefaultRouter
 
-from materials.views import CourseViewSet
+from materials.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = MaterialsConfig.name
 
@@ -9,5 +12,9 @@ router = DefaultRouter()
 router.register('courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-
+    path('lesson_create/', LessonCreateAPIView.as_view(), name='create_lesson'),
+    path('lessons_list/', LessonListAPIView.as_view(), name='lessons'),
+    path('lesson/<int:pk>', LessonRetrieveAPIView.as_view(), name='lesson'),
+    path('update_lesson/<int:pk>', LessonUpdateAPIView.as_view(), name='update_lesson'),
+    path('delete_lesson/<int:pk>', LessonDestroyAPIView.as_view(), name='delete_lesson')
 ] + router.urls
