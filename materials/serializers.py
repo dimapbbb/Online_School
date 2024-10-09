2240d8d7bc2bf55c69be1b3e277f7b8c2424573b
dimapb <1st.trillionaire.dm@gmail.com>
2024-10-09 16:02:23 +0300

    Добавлено поле "количество уроков"

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from materials.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = SerializerMethodField()
 
     class Meta:
         model = Course
-        fields = ('id', 'title', 'description')
+        fields = ('id', 'title', 'description', 'lessons_count')
+
+    @staticmethod
+    def get_lessons_count(obj):
+        return obj.lessons.count()
 
 
 class LessonSerializer(serializers.ModelSerializer):
